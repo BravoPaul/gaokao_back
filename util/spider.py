7e5a8ffea32f7134782edee7d884ce4df312c151
BravoPaul <pankun199@gmail.com>
2020-07-20 22:04:42 +0800

    asdfasd

--- util/spider.py
+++ util/spider.py
@@ -535,10 +535,15 @@ def mul_thread_run(func, result_name=''):
 
 def main():
     # spider_all_university()
+    print('爬虫 spider_all_university_index')
     mul_thread_run(spider_all_university_index)
+    print('爬虫 spider_all_school_score')
     mul_thread_run(spider_all_school_score)
+    print('爬虫 spider_all_major_score')
     mul_thread_run(spider_all_major_score)
+    print('爬虫 spider_major_list')
     mul_thread_run(spider_major_list)
+    print('爬虫 spider_all_major_detail')
     mul_thread_run(spider_all_major_detail)
 
 
