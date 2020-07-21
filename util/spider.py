9d8907820d054326f95757c499d0da8f85e5584d
坤月 <pankun.pk@alibaba-inc.com>
2020-07-21 14:49:40 +0800

    dsfa

--- util/spider.py
+++ util/spider.py
@@ -114,6 +114,7 @@ def download_page_university_detail(url, **kargs):
             print('进度：:', kargs['page_num'])
         return result
     else:
+        print(soup.text)
         raise ValueError()
 
 
